96e450e0684e79467fdfd418ffce38193ddb70dc
Ken Lu <ken@seeice.com>
2013-05-27 09:12:32 -0400

    added rack base test for sinatra Fixed #13

--- test/my_app_test.rb
+++ test/my_app_test.rb
@@ -2,16 +2,25 @@ require File.expand_path '../../my_app.rb', __FILE__
 require 'test/unit'
 require 'rack/test'
 
+OUTER_APP = Rack::Builder.parse_file('config.ru').first
 class MyAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-    MyApp.new
+    #MyApp.new #as well as Sinatra::Application will not support the rack test
+    OUTER_APP
   end
 
   def test_default
     get '/'
     assert_include last_response.body, 'Article'
+    assert last_response.ok?
+  end
+  
+  def test_basic_authenticate
+    authorize 'ken', 'password'
+    get '/basic'
+    assert_equal last_response.body, 'basic secret'
     assert last_response.ok?
   end
 end
